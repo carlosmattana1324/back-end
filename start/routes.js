694cc80d9d1e2f04501f1ccbf1f7bd59b39aaee3
priguetto <possiblefall@hotmail.com>
2020-07-10 22:09:02 -0400

    Commit do back-and

--- start/routes.js
+++ start/routes.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const Route = use('Route')
-
 Route.resource('/imoveis', 'ImovelController').apiOnly().middleware('auth') 
 Route.post('/usuarios', 'UserController.create').middleware()
 Route.post('/login', 'SessionController.create').middleware()
